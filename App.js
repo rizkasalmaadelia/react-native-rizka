e29af332686aa626a56e43a189527bf813e3fbc8
rizkasalmaadelia <rizkasalmaadelia@gmail.com>
2018-08-29 20:29:42 +0700

    perjuanganku

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <Text>Nama : RIZKA SALMA ADELIA</Text>
+        <Text>No Absen : 33</Text>
+        <Text>Kelas : XI RPL 1</Text>
+        
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
